922bb2b04b48c9e1c25b2f6eaabb9254c49da6a4
Yang Kewen <41265511+yang0369@users.noreply.github.com>
2023-10-29 15:07:40 +0800

    rename

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/")
+@app.route("/health")
+def index():
+    return 'index'
+
+
+@app.route("/home")
 def home():
     return render_template('home.html')
 
 
-# this is the entrance of the code
+# this is the entrance of the app
 if __name__ == '__main__':
     app.run(debug=True, port=5005)
+
+
+# deployment
+# https://render.com/docs/deploy-flask
